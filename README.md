1e2a5f657df61ea8d1fb59e96ee7f9e7a28a5e44
Ben <benhuang1024@gmail.com>
2024-01-28 16:35:24 +0800

    [main] run project

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ run
 ```shell
 ./vendor/bin/pint
 ```
+
+init run
+
+```shell
+php -d memory_limit=-1 bin/magento setup:install --base-url="http://swift.test" --db-host=127.0.0.1 --db-name=swift_test --db-user=root --db-password=password --admin-firstname=Ben --admin-lastname=Huang --admin-email=benhuang1024@gmail.com --admin-user=ben --admin-password=d3d34d33 --language=zh_Hans_CN --currency=CNY --timezone=Asia/Shanghai --use-rewrites=1 --cleanup-database --cache-backend=redis --cache-backend-redis-server=127.0.0.1 --cache-backend-redis-db=1 --session-save=redis --session-save-redis-host=127.0.0.1 --session-save-redis-db=0 --search-engine=elasticsearch7 --elasticsearch-host="127.0.0.1" --elasticsearch-port=9200 --elasticsearch-enable-auth=0 --elasticsearch-username=elastic --elasticsearch-index-prefix=swift
+```
---
+++ app/etc/env.php
@@ -0,0 +1,119 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_bu53ub'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'r2oN1dNFdEtnTY90pAqU14rPG0WW8Uof'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '1b1_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => '127.0.0.1',
+                    'database' => '1',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '1b1_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'efc386d9c8a039cedc3d5471a543c8fa'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'swift_test',
+                'username' => 'root',
+                'password' => 'password',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '0',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '6',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost',
+        'swift.test'
+    ],
+    'install' => [
+        'date' => 'Sun, 28 Jan 2024 08:30:45 +0000'
+    ]
+];
