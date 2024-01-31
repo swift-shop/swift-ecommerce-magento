d55b94e049322618c34f433e21e9abf6a2f6e699
Ben <benhuang1024@gmail.com>
2024-01-31 14:01:58 +0800

    [main] add phpstan

--- app/code/Tool/Data/Services/RequestService.php
+++ app/code/Tool/Data/Services/RequestService.php
@@ -4,4 +4,15 @@ namespace Tool\Data\Services;
 
 class RequestService
 {
+    public function getJson()
+    {
+        try {
+            $a = 1 / 3;
+            $requestData = [];
+            $params = file_get_contents("php://input");
+            $requestData = json_decode($params, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+        }
+        return $requestData;
+    }
 }
